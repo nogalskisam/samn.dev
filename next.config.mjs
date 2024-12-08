913ae6c68426221d9e837c9a83b7dccede818a79
Samuel Nogalski <sam.nogalski@axol.team>
2024-12-08 15:27:35 +0000

    Update config files

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
   output: "export",
   // Configure base path for GitHub Pages deployment
   basePath: process.env.BASE_URL,
+  assetPrefix: process.env.ASSET_PREFIX,
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
