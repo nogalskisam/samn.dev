9bf2acd07e4b5df96594e47ec2fc761485679fca
Samuel Nogalski <sam.nogalski@axol.team>
2024-12-08 15:37:52 +0000

    Remove basePath and assetPrefix from next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,11 @@
 import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  // Trim off `refs/heads` to get the branch name
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
 
 const nextConfig = {
   output: "export",
-  basePath: basePath,
-  assetPrefix: assetPrefix,
+  basePath: "",
+  assetPrefix: "",
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
